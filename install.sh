21436c6a4dedd9cbc5d182b45af80d39cef37a72
Alex <95647169+root616C6578@users.noreply.github.com>
2025-08-16 19:06:34 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,9 @@ sudo cp -r torsearch/ /opt/torsearch
 
 sudo ln -s /opt/torsearch/searcher.py /usr/local/bin/searcher
 sudo chmod +x /opt/torsearch/searcher.py
+
 echo -e "\e[32mThe installation was completed!\e[0m"
 echo -e "\e[33mPlease input  in terminal: searcher --help\e[0m"
+
 cd ..
 sudo rm -rf  torsearch
