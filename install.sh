02f7eb52b4b42833fb872571f9b62ce039c99bd9
Alex <95647169+root616C6578@users.noreply.github.com>
2025-08-16 18:42:07 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt  update -y &&sudo  apt install  tor -y
+
+if pip3 install  -r requirements.txt; then
+echo "reqirements was installled"
+else 
+pip3 install -r requirements.txt --break-system-packages
+echo  "reqirements was installled"
+fi
+
+cd ..
+sudo cp -r torsearch/ /opt/torsearch
+
+sudo ln -s /opt/torsearch/searcher.py /usr/local/bin/searcher
+sudo chmod +x /opt/torsearch/searcher.py
+sudo rm -rf  torsearch
+
+echo "The installation was completed!"
+echo"Please input  in terminal: torsearch --help"
